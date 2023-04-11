436b2465fa5e52aa1cd8b8319e1ba1f3224ab8be
Christopher Kufis <christopherkufis@icloud.com>
2023-04-11 15:40:01 -0700

    backend testing -auto

--- backend/public/index.php
+++ backend/public/index.php
@@ -1,11 +1,10 @@
 <?php
 require_once 'backend/app/router/router.php';
-
+require_once 'backend/app/controllers/userController.php';
 session_start();
 
 $router = new Router();
 $router->addRoute('POST','/users/login',['userController' => 'backend/app/controllers/userController.php',
 'action' => 'authenticate'
 ]);
-echo json_encode('hi')
 ?>
