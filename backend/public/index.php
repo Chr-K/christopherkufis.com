921be33e928ab1c2603e661aadc28003c2b136ae
Christopher Kufis <christopherkufis@icloud.com>
2023-04-11 15:14:29 -0700

    backend testing -auto

--- backend/public/index.php
+++ backend/public/index.php
@@ -1,7 +1,6 @@
 <?php
 require_once 'backend/app/router/router.php';
-header('Content-Type: text/html; charset=utf-8');
-header('Content-Type: application/x-www-form-urlencoded');
+
 session_start();
 
 $router = new Router();
