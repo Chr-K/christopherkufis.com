8467b61c9d16d897e50d481bc125038b5d7b1fed
Christopher Kufis <christopherkufis@icloud.com>
2023-04-12 16:04:42 -0700

    backend testing -auto

--- backend/public/index.php
+++ backend/public/index.php
@@ -2,7 +2,7 @@
 header("Content-Type: multipart/form-data");
 header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Headers: Content-Type");
-
+header("Access-Control-Allow-Methods: POST");
 require_once '/var/www/html/christopherkufis.com/backend/app/router/router.php';
 require_once '/var/www/html/christopherkufis.com/backend/app/controllers/userController.php';
 session_start();
--- frontend/src/routes/Components/Pages/Login/Login.tsx
+++ frontend/src/routes/Components/Pages/Login/Login.tsx
@@ -87,7 +87,7 @@ export default function Login(){
             const formData = new FormData()
             formData.append('email','ckufislogin@icloud.com')
             formData.append('password','basic123')
-        await  fetch('/users/login',{
+        await  fetch('/backend/public/index.php/users/login',{
                 method: 'POST',
                 body: formData,
             })
