fb20d5a9cce7dc660062936330c5dfc9ebe3ccd5
Christopher Kufis <christopherkufis@icloud.com>
2023-04-12 07:51:17 -0700

    backend testing -auto

--- frontend/src/routes/Components/Pages/Login/Login.tsx
+++ frontend/src/routes/Components/Pages/Login/Login.tsx
@@ -87,7 +87,7 @@ export default function Login(){
             const formData = new FormData()
             formData.append('email','ckufislogin@icloud.com')
             formData.append('password','basic123')
-        await  fetch('/api/users/login',{
+        await  fetch('/users/login',{
                 method: 'POST',
                 body: formData,
             })
