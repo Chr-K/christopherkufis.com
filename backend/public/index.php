1bea654f1b5f70cdb04c75b3995f8a091c2c653d
Christopher Kufis <christopherkufis@icloud.com>
2023-04-12 16:48:59 -0700

    backend testing -auto

--- backend/public/index.php
+++ backend/public/index.php
@@ -3,6 +3,7 @@ header("Content-Type: multipart/form-data");
 header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Headers: Content-Type");
 header("Access-Control-Allow-Methods: POST");
+set_include_path(get_include_path() . PATH_SEPARATOR . '/var/www/html/christopherkufis.com/backend');
 require_once '/var/www/html/christopherkufis.com/backend/app/router/router.php';
 require_once '/var/www/html/christopherkufis.com/backend/app/controllers/userController.php';
 session_start();
