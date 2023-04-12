fb357a0ad5448de86480cd27cbb6bf4f1fa0f2c8
Christopher Kufis <christopherkufis@icloud.com>
2023-04-12 07:24:24 -0700

    backend testing -auto

--- backend/public/index.php
+++ backend/public/index.php
@@ -4,7 +4,7 @@ header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Headers: Content-Type");
 
 
-echo json_encode($_POST['email']);
+echo $_POST['email'];
 
 /*
 require_once 'backend/app/router/router.php';
