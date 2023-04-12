1c805232af570d4a94406bfb08f01ed3c4322cc1
Christopher Kufis <christopherkufis@icloud.com>
2023-04-12 07:34:02 -0700

    backend testing -auto

--- backend/public/index.php
+++ backend/public/index.php
@@ -3,13 +3,6 @@ header("Content-Type: Multipart/form-data");
 header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Headers: Content-Type");
 
-if(isset($_POST['email'])){
-    echo json_encode('isset');
-}
-else {
-    echo json_encode('isnotset');
-}
-/*
 require_once 'backend/app/router/router.php';
 require_once 'backend/app/controllers/userController.php';
 session_start();
@@ -18,7 +11,5 @@ $router = new Router();
 $router->addRoute('POST','/users/login',['userController' => '../backend/app/controllers/userController.php',
 'action' => 'authenticate'
 ]);
-
- */
 ?>
 
