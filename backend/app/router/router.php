a79312ca7dc9d14d427f308e91ca75581f3af345
Christopher Kufis <christopherkufis@icloud.com>
2023-04-12 17:02:05 -0700

    backend testing -auto

--- backend/app/router/router.php
+++ backend/app/router/router.php
@@ -7,26 +7,26 @@ class Router{
         $this->routes[] = ['method' => $method,'path'=>$path,'handler'=>$handler];
     }
 
-    public function handleRequest(){
+    public function handleRequest() {
         $method = $_SERVER['REQUEST_METHOD'];
         $uri = $_SERVER['REQUEST_URI'];
-
-        foreach($this->routes as $route){
-            if($route['method'] === $method && preg_match('#^'.$route['path'] . '$#',$uri,$matches)){
+    
+        foreach ($this->routes as $route) {
+            if ($route['method'] === $method && preg_match('#^' . $route['path'] . '$#', $uri, $matches)) {
                 array_shift($matches);
                 $controllerName = $route['handler']['controller'];
                 $actionName = $route['handler']['action'];
                 $controller = new $controllerName();
-                echo json_encode('matches');
-                return $controller->$actionName(...$matches);
+                $response = $controller->$actionName(...$matches);
+                echo json_encode($response);
+                return;
             }
         }
-
-
-
+    
         http_response_code(404);
         echo json_encode('404 Not Found');
     }
+    
 }
 
 
