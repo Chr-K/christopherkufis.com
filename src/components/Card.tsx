e69b33764b778d802746f32b69d65f6612fe1512
Chr-K <104398517+Chr-K@users.noreply.github.com>
2023-12-31 07:39:54 -0800

    test

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -2,13 +2,10 @@ import '../styles/card.css'
 import { CardProps } from './CardProps'
 
 export default function Card(card : CardProps){
-    const {title,subtitle,content,created,updated} = card
-    console.log(content + created + updated)
     return(
-      
     <div className="card">
-        <h1>{title}</h1>
-        <h2>{subtitle}</h2>
+        <h1>{card.title}</h1>
+        <h2>{card.subtitle}</h2>
     </div>
     
     )
