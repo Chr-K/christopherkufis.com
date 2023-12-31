d77e3fe198573ac3445b88c8ac29bb016903a013
Chr-K <104398517+Chr-K@users.noreply.github.com>
2023-12-31 06:58:25 -0800

    test

--- src/routes/home.tsx
+++ src/routes/home.tsx
@@ -2,7 +2,10 @@ import { useEffect } from 'react'
 import Card from '../components/Card'
 export default function Home(){
     useEffect(()=>{
-        fetch('https://api.christopherkufis.com/articles')
+        fetch('https://api.christopherkufis.com/articles',{
+            method:"GET",
+            mode:"cors"
+        })
         .then((res)=>{
             if(!res.ok){
                 throw new Error(`HTTP error! Status: ${res.status}`)
