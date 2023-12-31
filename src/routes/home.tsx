8b8619cd2af89f984cf5f216083f2c558d8e44cc
Chr-K <104398517+Chr-K@users.noreply.github.com>
2023-12-31 06:56:30 -0800

    api

--- src/routes/home.tsx
+++ src/routes/home.tsx
@@ -1,5 +1,19 @@
+import { useEffect } from 'react'
 import Card from '../components/Card'
 export default function Home(){
+    useEffect(()=>{
+        fetch('https://api.christopherkufis.com/articles')
+        .then((res)=>{
+            if(!res.ok){
+                throw new Error(`HTTP error! Status: ${res.status}`)
+            }
+
+            return res.json()
+        })
+        .then((res)=>{
+            console.log(res)
+        })
+    },[])
     return(
         <>
         <Card title='test' subtitle='test'></Card>
