c3d59c295311007441e99913dc038eb19d7e8a70
Chr-K <104398517+Chr-K@users.noreply.github.com>
2024-01-01 16:22:59 -0800

    test

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -5,6 +5,7 @@ import { useNavigate } from 'react-router-dom'
 export default function Card(card : CardProps){
     const navigate = useNavigate()
     function handleClick(){
+        console.log(card.ID)
         navigate(`/article/${card.ID}`)
     }
     console.log(card)
