f13b712889be0a9343437a960b18a59f4194492b
Chr-K <104398517+Chr-K@users.noreply.github.com>
2024-01-01 16:34:00 -0800

    test

--- src/main.tsx
+++ src/main.tsx
@@ -28,7 +28,7 @@ const router = createBrowserRouter([
       path:'/article/:ID',
       element:<Article></Article>,
       loader:async (ID)=>{
-        console.log(ID)
+        console.log(ID.params.ID)
         console.log(JSON.stringify(ID))
         return await fetch(`https://api.christopherkufis.com/article/${ID}`)
       }
