a94da03ce918bcb845d1f8689a699f9b5157e76c
Chr-K <104398517+Chr-K@users.noreply.github.com>
2024-01-01 17:19:41 -0800

    test

--- src/components/Article.tsx
+++ src/components/Article.tsx
@@ -1,18 +1,10 @@
-import { useLoaderData } from "react-router-dom"
 
 export default function Article(){
-    const articleData:HTMLElement | null = useLoaderData() as HTMLElement
-    console.log(articleData)
     let content
-    if(articleData){
-        content = articleData
-    }
-    else{
         content = <div>Nothing to see here</div>
-    }
     return(
         <>
-                {content}
+        {content}
         </>
     )
 }
