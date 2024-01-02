36b0e6f3632fd826a6b80c34d6721364da4a58b5
Chr-K <104398517+Chr-K@users.noreply.github.com>
2024-01-01 17:58:40 -0800

    test

--- src/components/Article.tsx
+++ src/components/Article.tsx
@@ -2,16 +2,8 @@ import { useLoaderData } from "react-router-dom"
 import { CardProps } from "./Types/CardProps"
 export default function Article(){
     const data = useLoaderData() as Array<CardProps>
-    const articleContent = document.getElementById('article-content')
-
-        if(data.length>0){
-            console.log(data[0].content)
-            if(articleContent){
-                articleContent.innerHTML = data[0].content
-            }    
-        }
 
         return(
-        <div id="article-content"></div>
+            JSON.parse(JSON.stringify(data))
         )
 }
