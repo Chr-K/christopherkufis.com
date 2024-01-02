95b48b213341747a0e823b5d4eac6ff2ceceec29
Chr-K <104398517+Chr-K@users.noreply.github.com>
2024-01-01 17:22:08 -0800

    test

--- src/components/Article.tsx
+++ src/components/Article.tsx
@@ -1,10 +1,13 @@
+import { useLoaderData } from "react-router-dom"
 
 export default function Article(){
+    const data = useLoaderData()
+    console.log(data)
     let content
+
         content = <div>Nothing to see here</div>
     return(
         <>
-        {content}
         </>
     )
 }
