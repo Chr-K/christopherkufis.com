6b7d74fd229132a93416eea60a2d78649fb921ef
Chr-K <104398517+Chr-K@users.noreply.github.com>
2024-01-01 17:35:40 -0800

    test

--- src/components/Article.tsx
+++ src/components/Article.tsx
@@ -2,7 +2,7 @@ import { useLoaderData } from "react-router-dom"
 export default function Article(){
     const data = useLoaderData() as Array<Object>
         if(data.length>0){
-            console.log(data)
+            console.log(data[0])
         }
 
     return(
