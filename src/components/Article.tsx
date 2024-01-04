45921bcde7a73dc2f50e2d6c22920ecbbc1b0a2c
Chr-K <104398517+Chr-K@users.noreply.github.com>
2024-01-04 03:10:40 -0800

    test

--- src/components/Article.tsx
+++ src/components/Article.tsx
@@ -1,8 +1,9 @@
 import { useLoaderData } from "react-router-dom"
 import { CardProps } from "./Types/CardProps"
+import { useEffect } from "react"
+const data = useLoaderData() as Array<CardProps>
 
 export default function Article(){
-    const data = useLoaderData() as Array<CardProps>
     console.log(data)
         return(
             <div dangerouslySetInnerHTML={{__html: data[0].content}}></div>
