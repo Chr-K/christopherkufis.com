537882bca03ea819fbbe05546fe10196941f3946
Chr-K <104398517+Chr-K@users.noreply.github.com>
2024-01-07 14:57:21 -0800

    test

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -4,11 +4,14 @@ import { useNavigate } from 'react-router-dom'
 
 export default function Card(card : CardProps){
     const navigate = useNavigate()
+    const date = new Date(card.created)
+    const cardDate = (date.getMonth()+1).toString() + "-" + date.getDate() + "-" + date.getFullYear().toString()
     function handleClick(){
         navigate(`/article/${card.ID}`)
     }
     return(
     <div className="card" onClick={handleClick}>
+        <p className='card-date'>{cardDate}</p>
         <p className='card-title'> {card.title} </p>
         <p className='card-content'> {card.subtitle} </p>
         <p className='read-link'>Click To Read</p>
